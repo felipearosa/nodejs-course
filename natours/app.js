d81da247c822f1e17c021dac250474319df35391
Felipe Arosa <felipe.arosa@gmail.com>
2022-09-28 18:34:21 -0300

    created post method and finish start

--- natours/app.js
+++ natours/app.js
@@ -4,8 +4,11 @@ const app = express();
 
 app.get('/', (req,res) => {
   res.status(200).json({ message: "hello from the server side!", app: "Natours" });
-})
+});
 
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
+});
 
 const port = 3000;
 app.listen(port, () => {
