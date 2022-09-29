9d9325d066b5a77d153aafc632153fd3d0199f4c
Felipe Arosa <felipe.arosa@gmail.com>
2022-09-29 11:33:03 -0300

    added get request handler

--- natours/app.js
+++ natours/app.js
@@ -1,13 +1,27 @@
+const fs = require('fs')
 const express = require('express');
+const res = require('express/lib/response');
 
 const app = express();
 
-app.get('/', (req,res) => {
-  res.status(200).json({ message: "hello from the server side!", app: "Natours" });
-});
+// app.get('/', (req,res) => {
+//   res.status(200).json({ message: "hello from the server side!", app: "Natours" });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'sucess',
+    length: tours.length,
+    data: {
+      tours
+    }
+  });
 });
 
 const port = 3000;
