d9ab9f549765cd3ae237ff47b2509e5d72d43d90
Felipe <felipe.arosa@gmail.com>
2022-10-04 11:22:52 -0300

    updated the createTour function

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,15 +1,5 @@
 const Tour = require('./../models/tourModel')
 
-exports.checkBody = (req, res, next) => {
-  if(!req.body.data.name || !req.body.data.price){
-    return res.status(404).json({
-      status: 'error',
-      message: 'invalid'
-    })
-  }
-  next();
-}
-
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'sucess',
@@ -40,13 +30,22 @@ exports.getTour = (req, res) => {
   // })
 };
 
-exports.createTour = (req ,res) => {
-  res.status(201).json({
-    status: "sucess",
-    // data: {
-    //   tour: newTour
-    // }
-  })
+exports.createTour = async (req ,res) => {
+  try{
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: "sucess",
+      data: {
+        tour: newTour
+      }
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    })
+  }
 };
 
 exports.updateTour = (req ,res) => {
