da971fdd7624b4d717f54ac1fbf516f0aecae67e
Felipe <felipe.arosa@gmail.com>
2022-10-04 09:42:55 -0300

    addded tourSchema and created model

--- natours/server.js
+++ natours/server.js
@@ -11,7 +11,25 @@ mongoose.connect(DB, {
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true
-}).then(con => console.log('Database successful'))
+}).then(con => console.log('Database successful'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "You need to add a name"],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, "Price needs to be set"]
+  }
+});
+
+const Tour = new mongoose.model('Tour', tourSchema)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
