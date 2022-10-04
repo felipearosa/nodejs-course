b5269be8943b0220b0991aaff710f3eef0f4a33e
Felipe <felipe.arosa@gmail.com>
2022-10-04 11:33:12 -0300

    updated getAllTours and getTours

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,33 +1,39 @@
 const Tour = require('./../models/tourModel')
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'sucess',
-    requestedAt: req.requestTime,
-    // length: tours.length,
-    // data: {
-    //   tours
-    // }
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'sucess',
+      requestedAt: req.requestTime,
+      length: tours.length,
+      data: {
+        tours
+      }
+    });
+  } catch (err){
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    })
+  }
 };
 
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  // const tour = tours.find(el => el.id === id);
-
-  // if(!tour){
-  //   return res.status(404).json({
-  //     status: 'failed',
-  //     message: 'Tour not found'
-  //   });
-  // };
-
-  // res.status(200).json({
-  //   status: "sucess",
-  //   data: {
-  //     tour
-  //   }
-  // })
+exports.getTour = async (req, res) => {
+  try{
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: "sucess",
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.createTour = async (req ,res) => {
