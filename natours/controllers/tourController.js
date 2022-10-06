4690feb1ed8ef954ab9ac53971da15651b2a4874
Felipe <felipe.arosa@gmail.com>
2022-10-06 16:43:04 -0300

    created tour stats

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -99,3 +99,32 @@ exports.deleteTour = async (req ,res) => {
     });
   }
 };
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } }
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' }
+        }
+      },
+    ]);
+    res.status(200).json({
+      status: "sucess",
+      data: {
+        stats
+      }
+    });
+  } catch (error) {
+    console.log(error)
+  }
+}
