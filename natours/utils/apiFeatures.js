c9a9591fa385698a82bad2bb6818e7ec48bdc147
Felipe <felipe.arosa@gmail.com>
2022-10-06 15:58:51 -0300

    created filter function

--- natours/utils/apiFeatures.js
+++ natours/utils/apiFeatures.js
@@ -1,4 +1,23 @@
 class APIFeatures {
+  constructor(query, queryString){
+    this.query = query;
+    this.queryString = queryString;
+    console.log('whaaat')
+  }
+
+  filter(){
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['sort', 'page', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el])
+
+    // Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(lte|lt|gte|gt)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this.query;
+  }
 
 }
 
