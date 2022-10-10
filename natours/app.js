72243616ea44c5cc8d1a85303c4808b829daede9
Felipe <felipe.arosa@gmail.com>
2022-10-10 12:02:11 -0300

    created appError class

--- natours/app.js
+++ natours/app.js
@@ -1,8 +1,10 @@
 
 const express = require('express');
-const morgan = require('morgan')
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const morgan = require('morgan');
+
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 // const res = require('express/lib/response');
 // const { redirect } = require('express/lib/response');
 
