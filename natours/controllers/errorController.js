341bd2cf772cb7b4714a813c1b0f6f264de48d1f
Felipe <felipe.arosa@gmail.com>
2022-10-10 14:29:40 -0300

    different error responses depending on environment

--- natours/controllers/errorController.js
+++ natours/controllers/errorController.js
@@ -1,9 +1,40 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+
+const sendErrorProd = (err, res) => {
+  // operational error
+  if(err.isOperational){
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+
+    // programming or some other error not leaking error details to user
+  } else {
+    console.error('ERROR: ', err)
+
+    res.status(500).json({
+      status: 'error',
+      mesage: 'Something went wrong!'
+    })
+  }
+}
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development'){
+    sendErrorDev(err, res)
+  } else {
+    sendErrorProd(err, res);
+  }
+
 }
