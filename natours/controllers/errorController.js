723cf99ecebd80f5d163bc0a8f6e530825b0bb3e
Felipe <felipe.arosa@gmail.com>
2022-10-11 15:15:24 -0300

    handled duplicate fields error

--- natours/controllers/errorController.js
+++ natours/controllers/errorController.js
@@ -3,7 +3,15 @@ const AppError = require('./../utils/appError')
 const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value}`;
   return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.keyValue.name;
+  const message = `Duplicate field value: ${value}. Type some other name`;
+
+  return new AppError(message, 400);
 }
+
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -44,6 +52,7 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production'){
     let error = Object.assign(err)
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
 
     sendErrorProd(error, res);
   }
