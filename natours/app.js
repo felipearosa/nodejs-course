03c81d0d464ed78b0d4d0040b1d22374b4d9d804
Felipe <felipe.arosa@gmail.com>
2022-10-16 15:38:11 -0300

    added a limit to api requests

--- natours/app.js
+++ natours/app.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -15,6 +16,15 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests, try again later"
+});
+
+app.use('/api', limiter)
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
