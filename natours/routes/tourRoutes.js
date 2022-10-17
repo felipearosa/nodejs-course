38d8dfc26efe695703624eccac4f363a29d5ea94
Felipe <felipe.arosa@gmail.com>
2022-10-17 16:44:14 -0300

    refactored

--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const tourController = require('../controllers/tourController')
 const authController = require('../controllers/authController')
 const reviewController = require('../controllers/reviewController')
+const reviewRouter = require('./reviewRoutes')
 const router = express.Router();
 
 // router.param('id', tourController.checkId)
+
+router.use('/:tourId/reviews', reviewRouter)
 
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 router.route('/tours-stats').get(tourController.getTourStats)
@@ -12,6 +15,5 @@ router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan)
 router.route('/').get(authController.protect, tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
 
-router.route('/:tourId/reviews').post(authController.protect, authController.restrictTo('user'), reviewController.createReview);
 
 module.exports = router
