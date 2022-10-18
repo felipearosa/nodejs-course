4bc3c6ef68565cad8c33e154046442a58d795da7
Felipe <felipe.arosa@gmail.com>
2022-10-18 14:09:20 -0300

    added /me route

--- natours/controllers/userController.js
+++ natours/controllers/userController.js
@@ -20,6 +20,11 @@ exports.createUser = (req, res) => {
   })
 };
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id
+  next();
+}
+
 exports.updateMe = catchAsync(async (req, res, next) => {
   if(req.body.password || req.body.passwordConfirm){
     return next(new AppError('Not the place for updating pw, try "/updateMyPassword"', 400));
