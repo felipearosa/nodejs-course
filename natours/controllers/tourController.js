db5086a5c006832473e168ce8072956cdc561ac3
Felipe <felipe.arosa@gmail.com>
2022-10-19 18:38:02 -0300

    added query for tours within

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -91,3 +91,20 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
     }
   });
 });
+
+exports.getToursWithin = catchAsync(async (req, res, next) =>{
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+  console.log(distance, lat, lng, unit);
+
+  const tours = await Tour.find({ startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } } });
+
+  res.status(200).json({
+    status: "sucess",
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+})
--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -13,6 +13,8 @@ router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.ge
 router.route('/tours-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(authController.protect, authController.restrictTo('admin', 'lead-guide', 'guide'), tourController.getMonthlyPlan);
 
+router.route('/tours-within/:distance/center/:latlng/unit/:unit').get(tourController.getToursWithin);
+
 
 router.route('/').get(tourController.getAllTours).post(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.createTour);
 
