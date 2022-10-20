d5893f08dac246f54c034028a63503a3f431a521
Felipe <felipe.arosa@gmail.com>
2022-10-20 15:21:18 -0300

    extended base template

--- natours/app.js
+++ natours/app.js
@@ -74,9 +74,22 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.status(200).render('base', {
     tour: 'The Forest Hiker',
-    user: 'Felipe'
+    user: 'Felipe',
+    title: 'Exciting tours for adventurous people'
   });
-})
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours'
+  })
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker'
+  })
+});
 
 
 app.use('/api/v1/tours', tourRouter);
