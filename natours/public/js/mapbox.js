e7e377984ae54bd244299d12e821c03a9c2f8c65
Felipe <felipe.arosa@gmail.com>
2022-10-21 14:28:16 -0300

    added map to tour page

--- natours/public/js/mapbox.js
+++ natours/public/js/mapbox.js
@@ -1,3 +1,14 @@
 
 const locations = document.querySelector('#map').dataset.locations;
 console.log(locations)
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmVsaXBlYXJvc2EiLCJhIjoiY2w2cDk1cHVyMGlsdzNjcjNjeGxuOGxsdCJ9.qzjW6Sw47Lpbl7AHOFeWkA';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  // center: [-118.113491, 34.111745],
+  // zoom: 10,
+  // interactive: false
+});
