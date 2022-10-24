7d43ee284635f715d2deb68b7d7fe3b90e7cc567
Felipe <felipe.arosa@gmail.com>
2022-10-24 10:43:36 -0300

    fixed logging out

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -67,7 +67,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
 exports.logout = (req, res) => {
    res.cookie('jwt', 'loggedout', {
-    expires: new date(Date.now() + 10 * 1000),
+    expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true
    })
 
